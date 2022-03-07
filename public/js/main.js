aa55e1b5706d1c24ed464f3650c1198ea2f5c4f4
Divi <73816974+divi4@users.noreply.github.com>
2022-03-07 13:02:31 +1100

    Basic frontend setup

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+const form = document.querySelector(".calculator")
+const CURRENT_AGE_REQUIRED = "Please enter your current age"
+const RETIREMENT_AGE_REQUIRED = "Please enter your retirement age"
+const ANNUAL_SPEND_REQUIRED = "Please enter a value greater than 0"
+// CURRENT_ASSETS if no value, assume 0
+// MONTHLY_CONTRIBUTION if no value, assume 0
+// GROWTH_RATE if no value, assume 0
+// INTEREST_RATE if no value, assume 0
+
+form.addEventListener("submit", function(e) {
+  event.preventDefault();
+
+  let currentAgeValid = hasValue(form.elements["current-age"], CURRENT_AGE_REQUIRED);
+  let retirementAgeValid = hasValue(form.elements["retirement-age"], RETIREMENT_AGE_REQUIRED);
+  let annualSpendValid = hasValue(form.elements["annual-spend"], ANNUAL_SPEND_REQUIRED);
+  checkNumber(form.elements["current-assets"]);
+  checkNumber(form.elements["monthly-contribution"]);
+  checkNumber(form.elements["growth-rate"]);
+  checkNumber(form.elements["interest-rate"]);
+
+  if (currentAgeValid && retirementAgeValid && annualSpendValid) {
+    form.submit()
+  }
+})
+
+function checkNumber(input) {
+  if (input.value.trim() === "") {
+    return input.value = input.defaultValue;
+  }
+  return
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === "") {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, "", true);
+}
+
+function showMessage(input, message, type) {
+  const msg = input.parentNode.querySelector("small");
+  msg.innerText = message;
+  input.classname = type ? "success" : "error"
+  return type
+}
