c09c2342bf8625459f619b8e9c9ca60641903fe4
Divi <73816974+divi4@users.noreply.github.com>
2022-03-08 12:32:37 +1100

    Added basic FIRE calculation logic

--- controllers/calculator.js
+++ controllers/calculator.js
@@ -20,20 +20,25 @@ module.exports = {
     },
     postCalculator: async (req,res)=>{
         try{
+            // These values are being implicitly converted from a string to a number
             let currentAge = req.body["current-age"]
             let retirementAge = req.body["retirement-age"]
             let annualSpend = req.body["annual-spend"]
             let currentAssets = req.body["current-assets"]
             let monthlyContribution = req.body["monthly-contribution"]
-            let growthRate = req.body["growth-rate"]
-            let interestRate = req.body["interest-rate"]
-
-            let fireNumber = annualSpend * 25
+            let adjustedGrowthRate = (req.body["growth-rate"] - req.body["interest-rate"])/100
+            let interestRate = req.body["interest-rate"]/100
             let ageDifference = retirementAge - currentAge
 
-            let coastNumber = Math.pow(fireNumber * (1 + interestRate), (-1 * ageDifference))
-            
+            let fireNumber = annualSpend * 25
+            let coastNumber = fireNumber * Math.pow((1 + adjustedGrowthRate), (-1 * ageDifference))
+            let finalValue = currentAssets * Math.pow((1 + adjustedGrowthRate), ageDifference)
 
+            console.log("Fire Number: " + fireNumber)
+            console.log("Coast Number: " + coastNumber)
+            console.log("Final Value: " + finalValue)
+
+            console.log("Years to retirement: " + ageDifference)
 
             res.render('index.ejs')
         }catch(err){
