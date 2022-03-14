d3e42f1e04d686ec3eb22a8f0bcfe659a3da9c6e
Divi <73816974+divi4@users.noreply.github.com>
2022-03-15 00:31:39 +1100

    Deleted unecessary files

--- routes/calculator.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const calculatorController = require('../controllers/calculator')
-
-router.get('/', calculatorController.getCalculator)
-router.get('/results/:id', calculatorController.getResults)
-router.post('/post', calculatorController.postCalculator)
-
-module.exports = router
