e99c616283fd2dc609fd61653c63e0ea1b1ecf6c
Divi <73816974+divi4@users.noreply.github.com>
2022-03-16 00:40:08 +1100

    Default graph behaviour added, can clear graph now and cleaned up code in main.js

--- README.md
+++ README.md
@@ -11,17 +11,17 @@ A FIRE calculator adjusted to take into account Australian superannuation
 -Learnt difference between CommonJS and RequireJS  
 -Learnt how to set [CSP headers](https://content-security-policy.com/examples/)  
 
-## Things to do:  
--Be able to remove all data on graph before updating to new parameters  
--Provide default values to the graph to populate the graph when users first enter the page  
--Made site mobile-first, responsive  
+## Things to do:   
+-Take monthly contributions into account in calculations and show on graph  
+-Prettify the graph  
+-Make site mobile-first, responsive  
 -Bring in my randomise colour code and use it to assign a colour to each plot point  
 -Check if the session is the same and update users data on server if so instead of creating a new document  
 -Make a string parser for the numbers inputted so they show up in the results as 30,000 instead of 30000  
--Add logic to ejs files to alter text if user has more money in Super than their FIRE number  
--Take monthly contributions into account in calculations  
+-Add logic to alter text if user has more money in Super than their FIRE number   
 -Optimise index.ejs and results.ejs back into one file again   
 -Add calculations to determine pre-Super FIRE number  
+-Explain how the FIRE and CoastFIRE number is created  
 -Create a proper frontend  
 -Add option for user to compare two different strategies and view them both on the graph  
 -Add option for users to download their data as a spreadsheet and/or plain text  
--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-const form = document.querySelector(".calculator")
-const CURRENT_AGE_REQUIRED = "Please enter your current age"
-const RETIREMENT_AGE_REQUIRED = "Please enter your retirement age"
-const ANNUAL_SPEND_REQUIRED = "Please enter a value greater than 0"
-// CURRENT_ASSETS if no value, assume 0
-// MONTHLY_CONTRIBUTION if no value, assume 0
-// GROWTH_RATE if no value, assume 0
-// INTEREST_RATE if no value, assume 0
-
-
+// Default graph that shows upon loading page
 const ctx = document.getElementById('myChart');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
+        labels: ['30','31','32','33','34','35','36','37','38','39','40','41',
+                '42','43','44','45','46','47','48','49','50','51','52','53',
+                '54','55','56','57','58','59','60','61','62','63','64','65'],
         datasets: [{
-            label: '$ value',
-            backgroundColor: 'rgba(17, 140, 79, 0.2)',
-            fill: 'origin',
-            borderWidth: 1
+          label: "Net worth",
+          data: [40000,42800,45796,49001.72,52431.8404,56102.06922800001,
+                60029.21407396,64231.259059137206,68727.4471932768,
+                73538.3684968062,78686.05429158262,84194.07809199342,
+                90087.66355843293,96393.80000752326,103141.36600804988,
+                110361.26162861339,118086.5499426163,126352.60843859945,
+                135197.2910293014,144661.10140135253,154787.3784994472,
+                165622.49499440848,177216.0696440171,189621.1945190983,
+                202894.67813543518,217097.30560491566,232294.11699725973,
+                248554.70518706794,265953.5345501627,284570.28196867404,
+                304490.2017064812,325804.515825935,348610.83193375036,
+                373013.5901691129,399124.5414809508,427063.2593846174],
+          backgroundColor: "rgba(17, 140, 79, 0.2)",
+          fill: "origin",
+          borderWidth: 1
+        },
+        {
+          label: "Fire Number",
+          data: [750000,750000,750000,750000,750000,750000,750000,750000,750000,
+                750000,750000,750000,750000,750000,750000,750000,750000,750000,
+                750000,750000,750000,750000,750000,750000,750000,750000,750000,
+                750000,750000,750000,750000,750000,750000,750000,750000,750000],
+          backgroundColor: "rgba(252, 9, 5, 0.6)",
+          pointHitRadius: 0,
+          pointRadius: 0,
+          borderWidth: 3,
+          borderColor: "rgba(252, 9, 5, 0.6)"
         }]
     },
     options: {
@@ -34,6 +51,16 @@ const myChart = new Chart(ctx, {
     });
 
 
+// *************** MAIN ******************
+const form = document.querySelector(".calculator")
+const CURRENT_AGE_REQUIRED = "Please enter your current age"
+const RETIREMENT_AGE_REQUIRED = "Please enter your retirement age"
+const ANNUAL_SPEND_REQUIRED = "Please enter a value greater than 0"
+// CURRENT_ASSETS if no value, assume 0
+// MONTHLY_CONTRIBUTION if no value, assume 0
+// GROWTH_RATE if no value, assume 0
+// INTEREST_RATE if no value, assume 0
+
 form.addEventListener("submit", function(e) {
   event.preventDefault();
 
@@ -47,110 +74,14 @@ form.addEventListener("submit", function(e) {
 
   if (currentAgeValid && retirementAgeValid && annualSpendValid) {
     let docObject = getData()
-    updateData(docObject)
-    let [labels, plots] = createGraphData(docObject)
-    console.log(labels)
-    console.log(plots)
-    updateGraphData(myChart, labels, plots)
+    updateText(docObject)
+    let [ages, plots] = createGraphData(docObject)
+    updateGraphData(myChart, ages, plots, docObject.fireNumber)
   }
 })
 
-function updateData(docObject) {
-   document.querySelector(".a").innerHTML = `Your ${docObject.ageDifference} years
-    from retirement`
-   document.querySelector(".b").innerHTML = `At a real growth rate (growth - inflation)
-    of ${docObject.adjustedGrowthRate}% and a planned annual spendings of
-    ${docObject.annualSpend} this makes your FIRE number:
-    ${docObject.fireNumber}`
-   document.querySelector(".c").innerHTML = `The amount of money you need to put
-    into your Super today to reach ${ docObject.fireNumber} by age
-    ${docObject.retirementAge}, is:`
-   document.querySelector(".d").innerHTML = `${ docObject.coastNumber}`
-   document.querySelector(".e").innerHTML = `With a Super that's currently at
-    ${docObject.currentSuper}, you've got to put in
-    ${docObject.actualCoastNumber} more to reach your FIRE number by retirement`
-    document.querySelector(".e").innerHTML = `Monthly contributions: ${docObject.monthlyContribution}`
-}
 
-function getData() {
-  let ageDifference = form.elements["retirement-age"].value - form.elements["current-age"].value
-  let growthRate = form.elements["growth-rate"].value
-  let interestRate = form.elements["interest-rate"].value
-  let fireNumber = form.elements["annual-spend"].value * 25
-  let coastNumber = roundOff(fireNumber * Math.pow((1 + ((growthRate - interestRate)/100)), (-1 * ageDifference)), 2)
-
-  let docObject = {
-      currentAge: form.elements["current-age"].value,
-      retirementAge: form.elements["retirement-age"].value,
-      ageDifference: ageDifference,
-      annualSpend: form.elements["annual-spend"].value,
-      currentSuper: form.elements["current-super"].value,
-      monthlyContribution: form.elements["monthly-contribution"].value,
-      growthRate: form.elements["growth-rate"].value,
-      interestRate: form.elements["interest-rate"].value,
-      adjustedGrowthRate: roundOff((growthRate - interestRate), 1),
-      fireNumber: fireNumber,
-      coastNumber: coastNumber,
-      actualCoastNumber: roundOff(coastNumber - form.elements["current-super"].value, 2)
-    }
-
-    return docObject
-}
-
-
-function createGraphData(docObject) {
-  // Need to create an array of ages and plot points
-  let labels = []
-  let plots = [docObject.currentSuper, docObject.fireNumber]
-
-  for (let i = 0; i < docObject.ageDifference + 1; i++) {
-      labels[i] = i + Number(docObject.currentAge)
-  }
-
-  for (let i = 0; i < docObject.ageDifference + 1; i++) {
-    if (i === 0) {
-      // Put in logic here to add currentSuper and add one year's worth of
-      // monthlyContribution every year after the first
-      // If currentSuper is 0, first year is 0, and year's worth of monthly
-      // contribution will make 2nd year + real growth
-      plots[0] = Number(docObject.currentSuper)
-    } else {
-      console.log(plots[i - 1])
-      plots[i] = Number(docObject.currentSuper) * Math.pow((1 + docObject.adjustedGrowthRate/100), i)
-   }
-  }
-  return [labels, plots]
-}
-
-
-function updateGraphData(myChart, labels, plots) {
-  removeGraphData(myChart)
-  addGraphData(myChart, labels, plots)
-}
-
-
-function addGraphData(chart, labes, plots) {
-    labes.forEach((label) => {
-      chart.data.labels.push(label);
-    });
-    plots.forEach((plot) => {
-        chart.data.datasets[0].data.push(plot);
-    });
-    chart.update();
-}
-
-
-function removeGraphData(chart) {
-    chart.data.labels.forEach((label) => {
-      chart.data.labels.pop(label);
-    });
-    chart.data.datasets[0].data.forEach((plot) => {
-        chart.data.datasets[0].data.pop(plot);
-    });
-    chart.update();
-}
-
-
+// ********** Form validation functions *************
 function checkNumber(input) {
   if (input.value.trim() === "") {
     return input.value = input.defaultValue;
@@ -182,6 +113,102 @@ function showMessage(input, message, type) {
   msg.innerHTML = message;
   input.classname = type ? "success" : "error"
   return type
+}
+
+
+// ********** Graph and data calculations functions *************
+function getData() {
+  let ageDifference = form.elements["retirement-age"].value - form.elements["current-age"].value
+  let growthRate = form.elements["growth-rate"].value
+  let interestRate = form.elements["interest-rate"].value
+  let fireNumber = form.elements["annual-spend"].value * 25
+  let coastNumber = roundOff(fireNumber * Math.pow((1 + ((growthRate - interestRate)/100)), (-1 * ageDifference)), 2)
+
+  let docObject = {
+      currentAge: form.elements["current-age"].value,
+      retirementAge: form.elements["retirement-age"].value,
+      ageDifference: ageDifference,
+      annualSpend: form.elements["annual-spend"].value,
+      currentSuper: form.elements["current-super"].value,
+      monthlyContribution: form.elements["monthly-contribution"].value,
+      growthRate: form.elements["growth-rate"].value,
+      interestRate: form.elements["interest-rate"].value,
+      adjustedGrowthRate: roundOff((growthRate - interestRate), 1),
+      fireNumber: fireNumber,
+      coastNumber: coastNumber,
+      actualCoastNumber: roundOff(coastNumber - form.elements["current-super"].value, 2)
+    }
+
+    return docObject
+}
+
+
+function createGraphData(docObject) {
+  let ages = []
+  let plots = [docObject.currentSuper, docObject.fireNumber]
+
+  for (let i = 0; i < docObject.ageDifference + 1; i++) {
+      ages[i] = i + Number(docObject.currentAge)
+  }
+
+  for (let i = 0; i < docObject.ageDifference + 1; i++) {
+    if (i === 0) {
+      // Put in logic here to add currentSuper and add one year's worth of
+      // monthlyContribution every year after the first
+      // If currentSuper is 0, first year is 0, and year's worth of monthly
+      // contribution will make 2nd year + real growth
+      plots[0] = Number(docObject.currentSuper)
+    } else {
+      plots[i] = Number(docObject.currentSuper) * Math.pow((1 + docObject.adjustedGrowthRate/100), i)
+   }
+  }
+  return [ages, plots]
+}
+
+
+function updateGraphData(myChart, ages, plots, fireNumber) {
+  removeGraphData(myChart)
+  addGraphData(myChart, ages, plots, fireNumber)
+}
+
+
+function addGraphData(chart, ages, plots, fireNumber) {
+    ages.forEach((age) => {
+      chart.data.labels.push(age)
+    });
+    plots.forEach((plot) => {
+        chart.data.datasets[0].data.push(plot)
+        chart.data.datasets[1].data.push(fireNumber)
+    });
+
+    chart.update();
+}
+
+
+function removeGraphData(chart) {
+    chart.data.labels = []
+    chart.data.datasets[0].data = []
+    chart.data.datasets[1].data = []
+    chart.update();
+}
+
+
+//*************** Primitives parsers ***********
+function updateText(docObject) {
+   document.querySelector(".a").innerHTML = `Your ${docObject.ageDifference} years
+    from retirement`
+   document.querySelector(".b").innerHTML = `At a real growth rate (growth - inflation)
+    of ${docObject.adjustedGrowthRate}% and a planned annual spendings of
+    ${docObject.annualSpend} this makes your FIRE number:
+    ${docObject.fireNumber}`
+   document.querySelector(".c").innerHTML = `The amount of money you need to put
+    into your Super today to reach ${ docObject.fireNumber} by age
+    ${docObject.retirementAge}, is:`
+   document.querySelector(".d").innerHTML = `${ docObject.coastNumber}`
+   document.querySelector(".e").innerHTML = `With a Super that's currently at
+    ${docObject.currentSuper}, you've got to put in
+    ${docObject.actualCoastNumber} more to reach your FIRE number by retirement`
+    document.querySelector(".e").innerHTML = `Monthly contributions: ${docObject.monthlyContribution}`
 }
 
 
